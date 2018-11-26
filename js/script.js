070cac3e1299dcfeb8a85e9ab2e29d5c36b4e976
Angelo Martin <angeloqmartin@gmail.com>
2018-11-26 07:13:37 -0800

    added breakpoints changes to make responsiveness cleaned up js files add tile design and responsivenss

--- js/script.js
+++ js/script.js
@@ -6,64 +6,73 @@ function findResources(seletedResource,postalCode) {
     .then(responseJson => renderResources(responseJson))
     .catch(err => console.log(err))
 }
-
 function hideElements() {
     $('main').addClass('hide');
     $('header').addClass('hide');
 }
-
+// handles click function on the the navigation a tags
+$(".nav-list a").click(function() {
+    $('.js-eventResults').addClass('hide');
+    $('.resourceResults').addClass('hide');
+    $('main').removeClass('hide');
+    $('header').removeClass('hide');
+})
 function watchFindResourceForm() {
     $(".find-resources-form").on("submit", (e) => {
-    e.preventDefault();
-    let postalCode = $('.postalcode').val();
-    const seletedresource = $(".resource").val();
-    findResources(seletedresource, postalCode);
+        $('.resourceResults').removeClass('hide');
+        e.preventDefault();
+        let postalCode = $('.postalcode').val();
+        const seletedresource = $(".resource").val();
+        findResources(seletedresource, postalCode);
 })}
-
 function generateResourceList(responseJson) {
     const resourceName = responseJson.results.map(data => 
-        `<div class="js-results-item">
-        <h3>${data.name}</h3>
-        <p>${data.formatted_address}</p>
-        </div>`)
+        `
+        <div class="tile">
+        <h3>${data.name}<h3>
+            <div id="tile-title">
+            <p>address:<br><br>${data.formatted_address}</p>
+            </div>
+        </div> 
+        `)
     return resourceName;
 };
-
 function renderResources(responseJson) {
     hideElements();
     $('.resourceResults').html(generateResourceList(responseJson));
-    
 }
-// --------------------------------------------------------------------------
-
 function findEvents(postalCode) {
-    fetch(`https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?q=volunteer%20homeless&l=${postalCode}&within=40&units=miles&app_key="3RM2NQ6NFX4bJkg4"`)
+    fetch(`https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?q=volunteer%20homeless&l=${postalCode}&within=50&units=miles&app_key="3RM2NQ6NFX4bJkg4"`)
     .then(response => response.json())
     .then(responseJson => renderEvents(responseJson))
 }
-
 function watchFindEventsForm() {
     $(".find-events-form").on("submit", (e) => {
-    e.preventDefault();
-    let postalCode = $('.postalcode').val();
-    findEvents(postalCode);
+        $('.js-eventResults').removeClass('hide');
+        e.preventDefault();
+        const postalCode = $('.js-events-postalcode').val();
+        findEvents(postalCode);
 })}
-
 function generateEventsList(responseJson) {
-    const eventsList = responseJson.events.event.map(item => 
-        `<div class="js-event-item">
-        <h3>${item.title}</h3>
-        <p>${item.venue_address}</p>
-        </div>`)
-        
-    return eventsList;
-};
+    const eventsList = responseJson.events.event.map(item =>
+        `
+        <div class="tile">
+            <div id="tile-date"><h3>when:<br><br>${item.start_time}<h3></div>
+            <div id="tile-title">
+                <h3>${item.venue_name}<h3>
+                <h3>${item.title}<h3>
+                <p>${item.city_name}, ${item.region_name}</p>
+            </div>
+            <a href="${item.url}" target="_blank">GET INVOLVED</a>
+        </div> 
+        `)
 
+    return eventsList
+}
 function renderEvents(responseJson) {
     hideElements();
     $('.js-eventResults').html(generateEventsList(responseJson));
 }
-
 $(function () {
     watchFindResourceForm();
     watchFindEventsForm();
